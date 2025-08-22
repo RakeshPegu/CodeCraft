36ece34a8e0c76d1f32a42f66b4a21a98653ca31
RakeshPegu <rakeshpegu903@gmail.com>
2025-08-22 12:51:40 +0530

    correction in env file

--- frontend/react-app/src/lib/apiRequest.js
+++ frontend/react-app/src/lib/apiRequest.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const apiRequest =  axios.create({
-    baseURL:`${import.meta.env.VITE_API_URI}/api/v1`,
+    baseURL:${import.meta.env.VITE_API_URI},
     withCredentials:true
 })
